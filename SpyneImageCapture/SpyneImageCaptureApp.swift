e6f2ada31108f653c933373a17d9dd5f0a7196ee
AdityaKotasthane <ak8906@srmist.edu.in>
2024-11-12 11:14:35 +0530

    Initial Commit

---
+++ SpyneImageCapture/SpyneImageCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyneImageCaptureApp.swift
+//  SpyneImageCapture
+//
+//  Created by Adit07 on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SpyneImageCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpyneImageCaptureTests/SpyneImageCaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpyneImageCaptureTests.swift
+//  SpyneImageCaptureTests
+//
+//  Created by Adit07 on 12/11/24.
+//
+
+import Testing
+@testable import SpyneImageCapture
+
+struct SpyneImageCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
